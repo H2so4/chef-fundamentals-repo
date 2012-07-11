fe5229af902c395e7a01db77b5bfcda2b3557cae
jtimberman <joshua@opscode.com>
2012-07-11 16:57:45 -0600

    increment knife-workstation version

--- cookbooks/knife-workstation/metadata.rb
+++ cookbooks/knife-workstation/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "training@opscode.com"
 license          "Apache 2.0"
 description      "Configures a 'Knife Workstation' for Chef Training students"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "1.2.2"
 supports         "ubuntu"
